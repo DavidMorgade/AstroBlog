1e057304647bc39dc5562c08397ce734616e6050
DavidMorgade <davidmorgadegil@gmail.com>
2023-10-05 23:14:55 +0200

    fixed a lot of things, changing the UI completly, added links for social media, still building the header for mobile and prettier not working

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "CódigoMorga";
+export const SITE_DESCRIPTION = "Bienvenido al Blog de Código Morga, yo soy David Morgade, estudiante de Desarrollo de aplicaciones Multiplataforma, aquí podrás encontrar diferentes articulos relacionados con la informática en general y más en concreto con la programación y desarrollo de aplicaciones FullStack.";
