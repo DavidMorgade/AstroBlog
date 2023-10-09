9fdfd007f89a8c881e74e94d738d36d1fcf3cf95
DavidMorgade <davidmorgadegil@gmail.com>
2023-10-09 22:11:25 +0200

    started about.astro

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = "CódigoMorga";
 export const SITE_DESCRIPTION = "Bienvenido al Blog de Código Morga, yo soy David Morgade, estudiante de Desarrollo de aplicaciones Multiplataforma, aquí podrás encontrar diferentes articulos relacionados con la informática en general y más en concreto con la programación y desarrollo de aplicaciones FullStack.";
+
+export const SITE_ABOUT = "- Sobre mí";
+export const SITE_ABOUT_DESCRIPTION = "Soy un estudiante de Desarrollo de aplicaciones Multiplataforma, me gusta mucho la informática y la programación, por eso cree este blog, para poder compartir mis conocimientos y experiencias con el resto del mundo.";
